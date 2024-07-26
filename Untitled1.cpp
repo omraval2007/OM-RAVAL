6558095dcc2219e46f135b249a334ce503c9b05c
omraval2007 <om.jigarkumarraval24sbs@dronafoundation.edu.in>
2024-07-26 09:12:26 +0530

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+	printf("welcome to my profile\n");
+	printf("my name is om raval\ni am 17 year old\none of my hobby is do somthing interesting always\nmy favorite game is cricket\n");
+	printf("i am from bapunagar\nahmedabad\ngujrat\nidia,380024\n");
+	printf("my neighbor name is raj vaja\n");
+	printf("my coolege name is shanti bussiness school\ni am studing b.voc it in first year");
+}
